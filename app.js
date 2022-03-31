ae5c975998b8d1a53171034361869ad3c156ac68
Leander Gjerstad <leandergjerstad@gmail.com>
2022-03-31 13:07:35 +0200

    Did a few more chapters, still not finished

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const newPosts = [];
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,23 +19,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { starting: homeStartingContent, about: aboutContent, contact: contactContent, postTitle: newPosts[0].title, postBody: newPosts[0].body});
-  console.log(newPosts);
+  res.render("home", {
+    starting: homeStartingContent,
+    about: aboutContent,
+    contact: contactContent,
+    newPost: newPosts
+  });
+  ;
 })
 
 app.get("/about", function (req, res) {
-  res.render("about",{about: aboutContent} )
+  res.render("about", { about: aboutContent })
 })
 
 app.get("/contact", function (req, res) {
   res.render("contact", { contact: contactContent })
 })
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose", {})
 })
 
-app.post("/compose", function(req,res){
+app.get("/posts/:postname", function (req, res) {
+  let pagename = req.params.postname;
+  let lowerCased = lodash.lowerCase(pagename)
+  let kebabCased = lodash.kebabCase(lowerCased);
+  console.log(kebabCased);
+
+  let i = 0;
+  for (element in newPosts) {
+    if (newPosts[i].title === kebabCased) {
+      console.log("Match found!")
+    } else {
+      if (newPosts[i].title === lowerCased) {
+        console.log("Match found!")
+      } else {
+        console.log("Match not found...")
+      }
+    }
+    i++
+  }
+})
+
+app.post("/compose", function (req, res) {
   let composedPost = {
     title: req.body.postTitle,
     body: req.body.postBody
